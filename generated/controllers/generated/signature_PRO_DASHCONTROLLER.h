705f7f5c470089b57c421868fa44b015225df221
GitHub git update Action <action@github.com>
2024-08-25 16:41:03 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_PRO_DASHCONTROLLER.h
+++ generated/controllers/generated/signature_PRO_DASHCONTROLLER.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4053507771
-#define TS_SIGNATURE "rusEFI main.2024.08.24.PRO_DASHCONTROLLER.4053507771"
+#define SIGNATURE_HASH 4260771712
+#define TS_SIGNATURE "rusEFI main.2024.08.25.PRO_DASHCONTROLLER.4260771712"
