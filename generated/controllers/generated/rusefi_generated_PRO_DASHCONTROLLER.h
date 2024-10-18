9f708f9df1642f81af3557d951f41d80acc77b64
GitHub git update Action <action@github.com>
2024-10-18 16:43:36 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_PRO_DASHCONTROLLER.h
+++ generated/controllers/generated/rusefi_generated_PRO_DASHCONTROLLER.h
@@ -1758,7 +1758,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.10.17.PRO_DASHCONTROLLER.1167217120"
+#define TS_SIGNATURE "rusEFI main.2024.10.18.PRO_DASHCONTROLLER.1167217120"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
