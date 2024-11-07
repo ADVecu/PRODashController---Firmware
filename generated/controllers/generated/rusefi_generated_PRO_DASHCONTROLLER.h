4efd2e4b9e5f8cbc445a8da4f23988ed247e8f24
GitHub git update Action <action@github.com>
2024-11-07 16:43:41 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_PRO_DASHCONTROLLER.h
+++ generated/controllers/generated/rusefi_generated_PRO_DASHCONTROLLER.h
@@ -1295,7 +1295,7 @@
 #define SentEtbType_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3341906207
+#define SIGNATURE_HASH 3684352512
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1756,7 +1756,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.11.06.PRO_DASHCONTROLLER.3341906207"
+#define TS_SIGNATURE "rusEFI main.2024.11.07.PRO_DASHCONTROLLER.3684352512"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
